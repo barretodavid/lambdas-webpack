1d18514f7aa939fb9d37f6f4e4ba91f313b4efba
David Barreto <barretollano@gmail.com>
2021-10-27 12:24:28 -0400

    Changed library type to commonjs to be explicit

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { IgnorePlugin } = require('webpack');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { IgnorePlugin } = require("webpack");
 
 module.exports = {
   entry: {
-    ['lambda-a']: './src/lambda-a/index.ts',
-    ['lambda-b']: './src/lambda-b/index.ts',
+    ["lambda-a"]: "./src/lambda-a/index.ts",
+    ["lambda-b"]: "./src/lambda-b/index.ts",
   },
-  target: 'node',
+  target: "node",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]/index.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]/index.js",
     library: {
-      type: 'umd',
+      type: "commonjs",
     },
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: ['ts-loader'],
+        use: ["ts-loader"],
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
+    extensions: [".ts", ".js", ".json"],
   },
-  mode: 'production',
+  mode: "production",
   plugins: [
     new CleanWebpackPlugin(),
     new IgnorePlugin({
